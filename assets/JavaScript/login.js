51ae087f03544760b6579bb6de54c4acfd572d25
pedroteixeira1998 <pteixeira98@live.com>
2023-03-31 14:50:01 -0300

    Finalizar JavaScript página de login com validações

---
+++ assets/JavaScript/login.js
@@ -0,0 +1,49 @@
+const password = document.querySelector('#password');
+const button = document.querySelector('.button-login');
+const form = document.querySelector('.login-form');
+const eye = document.querySelector('.seepassword');
+
+function validacaoEmail(field) {
+    usuario = field.value.substring(0, field.value.indexOf("@"));
+    dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+    
+    if ((usuario.length >=1) &&
+        (dominio.length >=3) &&
+        (usuario.search("@")==-1) &&
+        (dominio.search("@")==-1) &&
+        (usuario.search(" ")==-1) &&
+        (dominio.search(" ")==-1) &&
+        (dominio.search(".")!=-1) &&
+        (dominio.indexOf(".") >=1)&&
+        (dominio.lastIndexOf(".") < dominio.length - 1)) {
+    }
+    else{
+    alert("E-mail invalido");
+    }
+    }
+
+const validateSenha = () =>{
+    if(password.value.length >= 8){
+        button.removeAttribute('disabled');
+}}
+
+const nextPage = (event) => {
+    event.preventDefault();
+
+    window.location = 'assets/pages/principal.html'
+}
+
+function togglePass() {
+    if(password.type == "password"){
+        password.type = "text";
+    }
+    else{
+        password.type = "password";
+    }
+}
+
+eye.addEventListener('click', togglePass);
+password.addEventListener('input', validateSenha);
+form.addEventListener('submit', nextPage);
+
+    
