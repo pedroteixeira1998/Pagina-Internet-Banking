a9af9d0b6242396c2a77e69621abb2e40f561942
pedroteixeira1998 <pteixeira98@live.com>
2023-04-12 22:03:42 -0300

    Criar página de consultas marcadas

--- assets/JavaScript/consultas.js
+++ assets/JavaScript/consultas.js
@@ -1,9 +1,23 @@
 const spanClient = document.querySelector('.client');
-const spanHour = document.querySelector('.spanhour');
-const spanDay = document.querySelector('.spanday');
-
-
-
 spanClient.innerHTML = localStorage.getItem('client');
-spanHour.innerHTML = localStorage.getItem('selectedHour');
-spanDay.innerHTML = localStorage.getItem('selectedDay');
+
+const table = document.querySelector('#consultasMarcadas');
+
+function createTab(){
+    const selectDay = localStorage.getItem('selectedDay');
+    const selectHour = localStorage.getItem('selectedHour');
+    const row = document.createElement('tr');
+    const cellDia = document.createElement('td');
+    const cellHora = document.createElement('td');
+    cellDia.textContent = selectDay;
+    cellHora.textContent = selectHour;
+    cellDia.classList.add('tab');
+    cellHora.classList.add('tab');
+    row.appendChild(cellDia);
+    row.appendChild(cellHora);
+    table.appendChild(row);
+};
+
+createTab();
+
+
--- assets/JavaScript/principal.js
+++ assets/JavaScript/principal.js
@@ -1,49 +1,44 @@
-//Nome do Cliente
+// Selecionar elementos do DOM
 const spanClient = document.querySelector('.client');
-
-var days = document.querySelector('.days');
-const openModalButton = document.querySelectorAll(".mark");
+spanClient.innerHTML = localStorage.getItem('client');
+const days = document.querySelector('.days');
+const openModalButton = document.querySelectorAll('.mark');
 const save = document.querySelector('#save');
 const modal = document.querySelector('#modal');
 const weekdays = document.querySelector('.weekdays');
+const fade = document.querySelector('#fade');
 
+// Definir variáveis e arrays
+let consultasMarcadas = [];
 
+const horas = [
+  '09:00',
+  '10:00',
+  '11:00',
+  '12:00',
+  '13:00',
+  '14:00',
+  '15:00',
+  '09:30',
+  '10:30',
+  '11:30',
+  '12:30',
+  '13:30',
+  '14:30',
+  '15:30'
+];
 
+const weekday = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'];
 
-//Array horas
-
-let consultasMarcadas =[];
-
-const horas = ['09:00', 
-'10:00', 
-'11:00', 
-'12:00', 
-'13:00', 
-'14:00', 
-'15:00',
-'09:30',
-'10:30',
-'11:30',
-'12:30',
-'13:30',
-'14:30',
-'15:30'];
-
-const weekday = ['Dom',
-'Seg',
-'Ter',
-'Qua',
-'Qui',
-'Sex',
-'Sab'];
-
+// Adicionar elementos de dias da semana ao calendário
 weekday.forEach(wd => {
   const divw = document.createElement('div');
   divw.classList.add('weekday');
   divw.textContent = wd;
   weekdays.appendChild(divw);
-})
+});
 
+// Adicionar elementos de horas ao modal
 horas.forEach(hour => {
   const div = document.createElement('div');
   div.classList.add('hour');
@@ -51,27 +46,24 @@ horas.forEach(hour => {
   modal.appendChild(div);
 });
 
+// Obter o número de dias do mês atual e criar elementos de dias do mês
+const date = new Date();
+const month = date.getMonth();
+const year = date.getFullYear();
+const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-
-// Obtenha o número de dias do mês atual
-var date = new Date();
-var month = date.getMonth();
-var year = date.getFullYear();
-var daysInMonth = new Date(year, month + 1, 0).getDate();
-
-// Crie as divs dos dias do mês
-for (var i = 1; i <= daysInMonth; i++) {
-  var day = document.createElement('div');
+for (let i = 1; i <= daysInMonth; i++) {
+  const day = document.createElement('div');
   day.classList.add('day');
   day.textContent = i;
   days.appendChild(day);
-}
+};
 
-// Seleciona todos os elementos do calendário que são dias
+// Selecionar elementos do calendário que são dias e horas
 const wdays = document.querySelectorAll('.day');
 const whorario = document.querySelectorAll('.hour');
 
-// Função para adicionar a classe 'card-mark' ao dia selecionado e remover dos demais dias
+// Funções para selecionar dia e hora e adicionar a classe 'card-mark'
 function selectDay(element) {
   wdays.forEach(day => {
     day.classList.remove('card-mark');
@@ -85,10 +77,12 @@ function selectHour(element) {
   });
   element.classList.add('card-mark');
 }
-// Adiciona um evento de clique em cada dia do calendário
+
+// Adicionar evento de clique em cada dia e hora do calendário
 wdays.forEach(day => {
   day.addEventListener('click', () => {
     selectDay(day);
+    toggleModal();
   });
 });
 
@@ -98,16 +92,21 @@ whorario.forEach(hour => {
   });
 });
 
+// Função para abrir e fechar o modal
+const toggleModal = () => {
+  modal.classList.toggle('hide');
+  fade.classList.toggle('hide');
+  };
 
-const toggleModal = () =>{
-  modal.classList.toggle("hide");
-  fade.classList.toggle("hide");
-};
+  fade.addEventListener('click', () => {
+      toggleModal();
+  });
 
-openModalButton.forEach((el) => {
-  el.addEventListener("click", () => toggleModal());
-});
+// openModalButton.forEach(el => {
+//   el.addEventListener('click', () => toggleModal());
+// });
 
+// Declaração de funções
 function resetPage() {
   const selectedDay = document.querySelector('.day.card-mark');
   const selectedHour = document.querySelector('.hour.card-mark');
@@ -122,16 +121,14 @@ function resetPage() {
 
   modal.classList.add('hide');
   fade.classList.add('hide');
-
-  
 }
-
 
 function reset(){
   consultasMarcadas =[];
   resetPage();
 }
 
+// Evento de clique no botão save
 save.addEventListener('click', function() {
   const selectedDate = document.querySelector('.day.card-mark').textContent;
   const selectedHour = document.querySelector('.hour.card-mark').textContent;
@@ -144,45 +141,29 @@ save.addEventListener('click', function() {
   consultasMarcadas.push(consulta);
 
   localStorage.setItem('consultas', JSON.stringify(consulta));
-
-  console.log(consultasMarcadas);
-
-  
-
   localStorage.setItem('selectedDay', selectedDate);
   localStorage.setItem('selectedHour', selectedHour);
 
   const table = document.querySelector('#consultasMarcadas');
 
-consultasMarcadas.forEach(consulta => {
-  const row = document.createElement('tr');
-  const cellDia = document.createElement('td');
-  const cellHora = document.createElement('td');
-  cellDia.textContent = consulta.dia;
-  cellHora.textContent = consulta.hora;
-  cellDia.classList.add('tab');
-  cellHora.classList.add('tab');
-  row.appendChild(cellDia);
-  row.appendChild(cellHora);
-  table.appendChild(row);
+  consultasMarcadas.forEach(consulta => {
+    const row = document.createElement('tr');
+    const cellDia = document.createElement('td');
+    const cellHora = document.createElement('td');
+    cellDia.textContent = consulta.dia;
+    cellHora.textContent = consulta.hora;
+    cellDia.classList.add('tab');
+    cellHora.classList.add('tab');
+    row.appendChild(cellDia);
+    row.appendChild(cellHora);
+    table.appendChild(row);
+  });
 
-  
-
+  reset();
 });
 
-
-  
-reset();
-
-
-
-  //alert(`Consulta: Dia: ${selectedDate}, as ${selectedHour} horas. Obrigado`)
-
-  //window.location = '../pages/confirmar.html'
-})
-
+// Criação da tabela de consultas marcadas
 const table = document.querySelector('#consultasMarcadas');
-
 consultasMarcadas.forEach(consulta => {
   const row = document.createElement('tr');
   const cellDia = document.createElement('td');
@@ -192,16 +173,7 @@ consultasMarcadas.forEach(consulta => {
   row.appendChild(cellDia);
   row.appendChild(cellHora);
   table.appendChild(row);
-
-
 });
-
-
-
-window.onload = () =>{
-  spanClient.innerHTML = localStorage.getItem('client');
-}
-
 
 
 
